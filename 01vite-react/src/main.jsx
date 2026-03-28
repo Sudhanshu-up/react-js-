1d26a0df6d278312605321628437ac37e1a501e3
Sudhanshu Chaudhari <sudhanshucode36@gmail.com>
2026-03-28 11:37:45 +0530

    understanding more

--- 01vite-react/src/main.jsx
+++ 01vite-react/src/main.jsx
@@ -1,9 +1,35 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import React from "react";
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  
-    <App />
-  
+function Myapp(){
+    return(
+        <h1>Hey this is MYAPP!!</h1>
+    )
+}
+const reactElement={
+    type:'a',
+    props:{
+        href:'https://google.com',
+        target:'_blank'
+    },
+    children:'click me to visit google'
+}
+
+const anotherElement=(
+    <a href="http://google.com" target="_blanck">
+        click to visit Google</a>
+)
+const eveluatedExpression= "its give final result "
+const onemoreElement= React.createElement(
+    'a',
+    {href:"http://google.com"},
+    'click for visiting',
+    eveluatedExpression
+
+)
+createRoot(document.getElementById('root'))
+.render(
+    onemoreElement
 );
