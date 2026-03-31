e512e1e65c1325624b28a572f5d798d30949d62f
Sudhanshu Chaudhari <sudhanshucode36@gmail.com>
2026-03-31 11:31:44 +0530

    props

--- 03tailwindpro/src/App.jsx
+++ 03tailwindpro/src/App.jsx
@@ -4,6 +4,10 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 import Card from './component/card'
+let profile={
+  username:"sudhanshu",
+  age:20,
+};
 
 function App() {
   const [count, setCount] = useState(0)
@@ -14,8 +18,8 @@ function App() {
     <h1 className="text-yellow-500">Test</h1>
     <p className='bg-green-300 p-4 rounded-xl text-black'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi, id laborum amet at atque quaerat necessitatibus vero repudiandae explicabo sint ducimus debitis dolore vitae iste aliquid sapiente facere nulla doloribus.</p>
     <Card/>
-    <Card/>
-    <Card/>
+    <Card username="abc"/>
+    <Card username ="sudhanshu" />
     
     
     </> 
