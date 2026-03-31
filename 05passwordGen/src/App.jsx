0c32a4662df1c977139e79c7cd5910083f2f67bc
Sudhanshu Chaudhari <sudhanshucode36@gmail.com>
2026-03-31 15:44:25 +0530

    add hover at button

--- 05passwordGen/src/App.jsx
+++ 05passwordGen/src/App.jsx
@@ -60,7 +60,9 @@ function App() {
           />
           <button
           onClick={copyPasswordToClipboard}
-           className="text-white bg-linear-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-linear-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-base text-sm px-4 py-2.5 text-center leading-5">Copy</button>
+           className="bg-blue-500 text-white px-6 py-2 rounded-lg 
+                hover:shadow-[0_0_20px_rgba(59,130,246,0.8)] 
+                transition duration-300">Copy</button>
 
         </div>
         <div className="flex text-sm gap-x-2">
