9243237611e2c034559a421b4f24975e3a69fe1e
Sudhanshu Chaudhari <sudhanshucode36@gmail.com>
2026-04-01 15:16:45 +0530

    new project

---
+++ 06currency/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react";
+
+function useCurrencyInfO(currency) {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    fetch(`https://open.er-api.com/v6/latest/${currency.toUpperCase()}`)
+      .then((res) => res.json())
+      .then((res) => {
+        setData(res.rates); // IMPORTANT CHANGE
+      })
+      .catch((err) => console.log("ERROR:", err));
+  }, [currency]);
+
+  return data;
+}
+
+export default useCurrencyInfO;
