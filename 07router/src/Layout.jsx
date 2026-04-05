6a833be70eed7cd9db3dc189ee450554bae37762
Sudhanshu Chaudhari <sudhanshucode36@gmail.com>
2026-04-05 09:56:02 +0530

    change header to Header

--- 07router/src/Layout.jsx
+++ 07router/src/Layout.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-import Header from './components/Header/header'
+import Header from './components/Header/Header.jsx'
 import Footer from './components/Footer/Footer'
 
 function Layout() {
